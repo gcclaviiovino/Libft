3ea98565e6ed20fb0bc621ef004ab6c65d656cc1
Lavinia Iovino <liovino@e3r11p3.42roma.it>
2024-12-12 16:56:19 +0100

    bentornata

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: liovino <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 16:54:28 by liovino           #+#    #+#             */
+/*   Updated: 2024/12/12 16:55:27 by liovino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
 	char			*f_str;
@@ -7,7 +19,7 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 	i = 0;
 	len = ft_strlen(s);
 	f_str = (char *) malloc(sizeof(char) * (len + 1));
-	if(!f_str)
+	if (!f_str)
 		return (NULL);
 	while (s[i] != '\0')
 	{
